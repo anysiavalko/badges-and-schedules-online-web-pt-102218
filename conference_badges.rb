4753a9f8607c63bc7879d0b4748dfa3febcac8f0
A. V <anysiavalko@gmail.com>
2018-10-30 02:47:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,5 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(name)
   conference_badges do
-    
+    name
   puts "Hello, my name is #{name}."
