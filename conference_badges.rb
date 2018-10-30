99473b5bf42d636361f494c50d0ec27d936a8307
A. V <anysiavalko@gmail.com>
2018-10-30 03:28:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(attendees)
   badges = []
   attendees.each do |attendee|
-    badges.push("Hello, my name is #{attendee}.")
+    badges.push(badge_maker(attendee))
     return badges
   end
 end
